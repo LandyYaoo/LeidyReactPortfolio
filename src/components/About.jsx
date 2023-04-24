0ca1a5ccda60bd59ed0b94d8d788702fbc3b5670
LandyYaoo <llpayaoo@hotmail.com>
2023-04-23 19:06:21 -0500

    Updating cv link 2

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ import Button from "./Button";
 const About = () => {
   const openCV = () => {
     window.open(
-      "https://drive.google.com/file/d/1u29dUzebQlla40y74E7gqCGoHGVy-lvN/view?usp=sharing",
+      "https://drive.google.com/file/d/1oqyaGBX3pefhJPFCuhOyK7_J6l5IMoLU/view?usp=sharing",
       "_blank"
     );
   };
