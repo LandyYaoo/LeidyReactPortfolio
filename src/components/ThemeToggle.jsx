5941489f4f3ac2f8daa0d1f1e1b76f262b0e704c
LandyYaoo <llpayaoo@hotmail.com>
2023-05-30 13:00:00 -0500

    cambio theme default

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -4,7 +4,7 @@ import stylesTheme from "../css/ThemeToggle.module.css";
 
 function ThemeToggle() {
   const [theme, setTheme] = useState(
-    localStorage.getItem("theme") || "light"
+    localStorage.getItem("theme") || "dark"
   );
 
   useEffect(() => {
