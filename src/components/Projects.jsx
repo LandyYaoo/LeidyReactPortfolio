e3236a47208c9a8bd1da18856700759c27bf5d39
LandyYaoo <llpayaoo@hotmail.com>
2023-05-30 13:11:20 -0500

    Changing links

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,16 +1,21 @@
 import Button from "./Button";
 import Card from "./Card";
 import { FaIdCard, FaMusic, FaUtensils } from "react-icons/fa";
+import { MdForest } from "react-icons/md";
 import stylesProjects from "../css/Projects.module.css"
 
 const Projects = () => {
   
-  const enlaces = ["https://landyyaoo.github.io/imaginedragons/", 
-  "https://github.com/LandyYaoo/imaginedragons",
-  "https://landyyaoo.github.io/Food-WebSite/", 
-  "https://github.com/LandyYaoo/Food-WebSite", 
-  "https://landyyaoo.github.io/Myportfolio/",
-  "https://github.com/LandyYaoo/Myportfolio"]
+  const enlaces = [
+    "https://landyyaoo.github.io/imaginedragons/",
+    "https://github.com/LandyYaoo/imaginedragons",
+    "https://landyyaoo.github.io/Food-WebSite/",
+    "https://github.com/LandyYaoo/Food-WebSite",
+    "https://leidy-pardo.netlify.app/",
+    "https://github.com/LandyYaoo/LeidyReactPortfolio",
+    "https://rutas-naturales.netlify.app",
+    "https://github.com/LandyYaoo/Reservas-Naturales-Blog",
+  ];
   
   const openLinks = (index) => {
       window.open(enlaces[index])
@@ -19,16 +24,32 @@ const Projects = () => {
   return (
     <article className={stylesProjects.all}>
       <section className={stylesProjects.projects} id="projects">
-          <h1>My projects</h1>
-          <section className={stylesProjects.proyectos}>
-            <Card typeCard={"odd"} iconCard={<FaMusic />} textCard={"Imagine dragons web page"} clickDeploy={() => openLinks(0)} clickRepo={() => openLinks(1)} />
-            <Card typeCard={"even"} iconCard={<FaUtensils />} textCard={"Food Web Page"} clickDeploy={() => openLinks(2)} 
-              clickRepo={() => openLinks(3)} />
-            <Card typeCard={"odd"} iconCard={<FaIdCard />} textCard={"My Portfolio"} clickDeploy={() => openLinks(4)} 
-              clickRepo={() => openLinks(5)} />
-          </section>
+        <h1>My projects</h1>
+        <section className={stylesProjects.proyectos}>
+          <Card
+            typeCard={"even"}
+            iconCard={<FaUtensils />}
+            textCard={"Food Web Page"}
+            clickDeploy={() => openLinks(2)}
+            clickRepo={() => openLinks(3)}
+          />
+          <Card
+            typeCard={"odd"}
+            iconCard={<FaIdCard />}
+            textCard={"My Portfolio"}
+            clickDeploy={() => openLinks(4)}
+            clickRepo={() => openLinks(5)}
+          />
+          <Card
+            typeCard={"even"}
+            iconCard={<MdForest />}
+            textCard={"Nature Reserves Blog"}
+            clickDeploy={() => openLinks(6)}
+            clickRepo={() => openLinks(7)}
+          />
+        </section>
       </section>
     </article>
-  )
+  );
 }
 export default Projects;
