2c985b4373c4813f6871eb094eca17a4b009b62e
LandyYaoo <llpayaoo@hotmail.com>
2023-06-09 14:37:36 -0500

    whatsapp

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,16 @@
-import styleButton from '../css/Button.module.css'
+import styleButton from "../css/Button.module.css";
 
 const Button = (props) => {
-  const { type, text, onClick } = props;
+  const { type, text, onClick, icon } = props;
   const isCommonButton = () => {
     return type === "common" ? styleButton.common : styleButton.alter;
-  }
+  };
   return (
     <button className={isCommonButton()} type={type} onClick={onClick}>
       {text}
+      {icon}
     </button>
   );
-}
+};
 
 export default Button;
-
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,19 +1,63 @@
-import { FaArrowRight } from "react-icons/fa";
+import { FaArrowRight, FaWhatsapp } from "react-icons/fa";
 import contactStyles from "../css/Contact.module.css";
 import Button from "./Button";
+import React, { useState } from "react";
 
-const Contact =()=>{
-  return(
+const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [description, setDescription] = useState("");
+
+  function enviarMensaje() {
+    const url = `https://api.whatsapp.com/send?phone=573014432138&text=Hola%20Leidy!%0A%0AMi%20nombre%20es:%0A${name}%0A%0AEmail:%0A${email}%0A%0AMensaje:%0A${description}%0A`;
+    window.open(url);
+  }
+
+  return (
     <section id="contact" className={contactStyles.contacto}>
       <h1>Contact Me</h1>
       <form action="#" className={contactStyles.formContact}>
-        <input type="text" placeholder="Your name here" className={contactStyles.yourname} />
-        <input type="text" placeholder="Your email here" className={contactStyles.youremail} />
-        <textarea name="description" id="description" cols="30" rows="10" placeholder="How can I help you?" className={contactStyles.description}></textarea>
-        <Button type="common" text="Send"/>
-        <h2><span className={contactStyles.span}><FaArrowRight /></span>  Let's Work Together</h2>
+        <input
+          type="text"
+          placeholder="Your name here"
+          className={contactStyles.yourname}
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Your email here"
+          className={contactStyles.youremail}
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <textarea
+          name="description"
+          id="description"
+          cols="30"
+          rows="10"
+          placeholder="How can I help you?"
+          className={contactStyles.description}
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        ></textarea>
+        <Button
+          type="common"
+          text={"Send "}
+          icon={<FaWhatsapp size={20} />}
+          onClick={enviarMensaje}
+        />
+        <h2>
+          <span className={contactStyles.span}>
+            <FaArrowRight />
+          </span>{" "}
+          Let's Work Together
+        </h2>
       </form>
     </section>
-  )
-}
+  );
+};
+
 export default Contact;
